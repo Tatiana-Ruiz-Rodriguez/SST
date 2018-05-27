4c5054ea1e0e6b313a388400e06487912fedcf63
Tatiana-Ruiz-Rodriguez <tatianaruiz@compucentroltda.com>
2018-05-26 19:50:25 -0500

    Interfz de Registro

---
+++ SST/SST/Views/IniciarSesion.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SST.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class IniciarSesion : ContentPage
+	{
+		public IniciarSesion ()
+		{
+			InitializeComponent (); 
+
+        }
+  
+        public async void SiguienteButton_Clicked(Object sender, EventArgs args)
+        {
+
+            await Navigation.PushAsync(new RegistroPersona());
+        }
+    }
+}
